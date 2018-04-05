3b8a4731695efda0698f1c917a7273e20793d728
Harry Tucker <skyclad_dragon@yahoo.com>
2018-04-04 22:24:43 -0500

    04April2018-2224hours

---
+++ BattleTank/Source/BattleTank/TankPlayerControler.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPlayerControler.h"
+#include"BattleTank.h"
+
+
+ATank * ATankPlayerControler::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerControler::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("Playercontroler is here!"));
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No Controlled tank!"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Playercontroling: %s"),*(ControlledTank->GetName()));
+	}
+}
